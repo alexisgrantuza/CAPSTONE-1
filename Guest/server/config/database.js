054ed40d645f3250b28e3bd3da03add9ca30293d
Alexis Grantuza <alexisgrantuza@gmail.com>
2025-06-07 04:21:00 +0800

    final update

--- Guest/server/config/database.js
+++ Guest/server/config/database.js
@@ -5,8 +5,12 @@ const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  process.env.DB_HOST,
-  process.env.DB_PORT
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false
+  }
 );
 
 // Test the connection
