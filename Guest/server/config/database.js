6e5009259efeb17d63f8e474835bb8ce028e3d7c
Alexis Grantuza <alexisgrantuza@gmail.com>
2025-06-07 03:15:26 +0800

    final update

--- Guest/server/config/database.js
+++ Guest/server/config/database.js
@@ -2,11 +2,11 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "guest_db",
-  process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "",
-  // process.env.DB_HOST || "dpg-d11dc9je5dus738l7880-a",
-  // process.env.DB_PORT || "5432",
+  process.env.DB_NAME || "sql12783467",
+  process.env.DB_USER || "sql12783467",
+  process.env.DB_PASSWORD || "PqYL2m8mHt",
+  process.env.DB_HOST || "sql12.freesqldatabase.com",
+  process.env.DB_PORT || "3306",
   {
     host: process.env.DB_HOST || "localhost",
     dialect: "mysql",
